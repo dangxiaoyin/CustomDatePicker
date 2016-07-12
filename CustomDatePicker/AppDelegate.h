7d1ae2de66ac0325ae0e9b7728b6badee60f8af2
dangxiaoyin <945979125@qq.com>
2016-07-12 11:16:42 +0800

    Initial Commit

---
+++ CustomDatePicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomDatePicker
+//
+//  Created by dangxy on 16/7/12.
+//  Copyright © 2016年 xproinfo.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomDatePicker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CustomDatePicker
+//
+//  Created by dangxy on 16/7/12.
+//  Copyright © 2016年 xproinfo.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
