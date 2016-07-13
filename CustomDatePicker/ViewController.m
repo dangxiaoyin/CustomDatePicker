2101cc80cf2a78f83f9204be5e4c45c2196771c5
dangxiaoyin <dangxy@promote.cache-dns.local>
2016-07-13 09:50:01 +0800

    自定义日期选择器
    pickerView

--- CustomDatePicker/ViewController.m
+++ CustomDatePicker/ViewController.m
@@ -20,9 +20,11 @@
 {
     [super viewDidLoad];
     
-    [self setBG];
+//    [self setBG];
     
-    [self filterCondition];
+//    [self filterCondition];
+    
+    [self stringTest];
 }
 
 -(void)setBG
@@ -30,6 +32,45 @@
     UIImageView *IMG = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, MAIN_WIDTH, MAIN_HEIGHT)];
     [IMG setImage:[UIImage imageNamed:@"IMG"]];
     [self.view addSubview:IMG];
+    
+}
+
+-(void)stringTest
+{
+    UILabel *content = [[UILabel alloc] initWithFrame:CGRectMake(20, 100, MAIN_WIDTH-40, 90)];
+    content.numberOfLines = 0;
+    content.text = @"改变字符串中指定字符的颜色,改变字符串中指定字符的颜色,改变字符串中指定字符的颜色,改变字符串中指定字符的颜色,改变字符串中指定字符的颜色!";
+    [self.view addSubview:content];
+    
+    NSRange range = [content.text rangeOfString:@"字符串"];
+    [self setTextColor:content FontNumber:[UIFont systemFontOfSize:13] AndRange:range AndColor:[UIColor orangeColor]];
+    
+//    for (int i = 0; i<[content.text length]-2; i++) {
+//        
+//        // 截取字符串中的每一个字符
+//        NSString *character = [content.text  substringWithRange:NSMakeRange(i, 3)];
+//        NSLog(@"character is %@",character);
+//        
+//        if ([character isEqualToString:@"字符串"]) {
+//            NSRange range = NSMakeRange(i, 3);
+//            //将字符串替换
+//            content.text = [content.text stringByReplacingCharactersInRange:range withString:@"YYY"];
+//        }
+//    }
+//    NSLog(@"%@",content.text );
+}
+
+
+//设置不同字体颜色
+-(void)setTextColor:(UILabel *)label FontNumber:(id)font AndRange:(NSRange)range AndColor:(UIColor *)vaColor
+{
+    NSMutableAttributedString *str = [[NSMutableAttributedString alloc] initWithString:label.text];
+    //设置字号
+    [str addAttribute:NSFontAttributeName value:font range:range];
+    //设置文字颜色
+    [str addAttribute:NSForegroundColorAttributeName value:vaColor range:range];
+    
+    label.attributedText = str;
 }
 
 -(void)filterCondition
